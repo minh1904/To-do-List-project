45adf4c3a0443697451cdf06fddbadde360b112c
Van Minh <kenmoster19@gmail.com>
2024-11-09 23:24:39 +0700

    2

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+const todoForm = document.querySelector('form');
+const todoInput = document.getElementById('todo-input');
+const todoListUl = document.getElementById('todo-list');
+
+let allTodo = [];
+todoForm.addEventListener('submit', function (e) {
+  alert('test');
+});
