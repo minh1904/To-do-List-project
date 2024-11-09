b732c538cd29a158f8c7d10607851c8486d60b50
Van Minh <kenmoster19@gmail.com>
2024-11-10 00:06:59 +0700

    di ngu

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 'use strict';
-const todoForm = document.querySelector('form');
-const todoInput = document.getElementById('todo-input');
-const todoListUl = document.getElementById('todo-list');
 
-let allTodo = [];
-todoForm.addEventListener('submit', function (e) {
-  alert('test');
+document.addEventListener('DOMContentLoaded', () => {
+  const todoForm = document.querySelector('form');
+  const todoInput = document.getElementById('todo-input');
+  const todoListUl = document.getElementById('todo-list');
+
+  let allTodos = [];
+  todoForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    addTodo();
+  });
+
+  const addTodo = function () {
+    const todoText = todoInput.value.trim();
+
+    if (todoText.length > 0) {
+      allTodos.push(todoText);
+      todoInput.value = '';
+    }
+  };
 });
